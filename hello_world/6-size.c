77f4c59afc1ecc04e7283e442a4cee6fbc9dfc6f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-17 12:50:23 -0700

    update6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -20,7 +20,8 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	
-	return 0;
-
+	return (0);
 }
+
+
 
