551c7701fbf4894caa15c48750ee5d717cf1f621
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-18 21:01:27 -0700

    update

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 	{
 		int n, n2;
 
-		for (n = 0; n < 8; n++)
+		for (n = 0; n < 9; n++)
 		{
 			
-			for (n2 = n + 1; n2 < 9; n2++)
+			for (n2 = n + 1; n2 < 10; n2++)
 			{
 			putchar((n % 10) + '0');
 			putchar((n2 % 10) + '0');
-		if (n == 8 && n2 == 9)
+		if (n == 9 && n2 == 10)
 			continue;
 		putchar(',');
 	putchar(' ');
