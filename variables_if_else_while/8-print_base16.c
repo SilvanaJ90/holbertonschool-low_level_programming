759fd23dfb1a74be6a825edcf67437cc2372e43c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-18 20:04:16 -0700

    update

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,10 +10,16 @@ int main(void)
 
 	{
 		int n;
+		char alphabet;
 
 		for (n = 0; n < 16; n++)
 			putchar((n % 16) + '0');
-		putchar('\n');
+
+		for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+                        putchar(alphabet);
+                putchar('\n');
+
+
 
 		return (0);
 	}
