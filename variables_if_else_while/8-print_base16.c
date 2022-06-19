17674f6b201a33176923841b89f29a48a640f37c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-18 19:47:24 -0700

    update

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+	{
+		int n;
+
+		for (n = 0; n < 16; n++)
+			putchar((n % 16) + '0');
+		putchar('\n');
+
+		return (0);
+	}
