914e0fcde19332ce5e94fb9750f0ad7c63bd86e8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-20 14:26:58 -0700

    update

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet  - entry point
+ *
+ * Description: Prints the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char l;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+		}
+			_putchar('\n');
+	}
+}
