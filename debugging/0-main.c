c7aae1f46759b6915490e5b765fd213236d64f58
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-21 13:14:39 -0700

    update

--- debugging/0-main.c
+++ debugging/0-main.c
@@ -1,14 +1,17 @@
-
 #include "main.h"
+
 /**
  * main - checks
- *
+ * @i -check
  * Return: 1 or 0 or -1
  */
 
 void main(void)
-{
 
+{
+	int i;
+	i = 0;
 	positive_or_negative();
+
 	return (0);
 }
