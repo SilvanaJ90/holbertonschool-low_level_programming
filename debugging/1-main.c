ee7a63dfde90c389839c22a1e8bdd3339a77d88e
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-21 14:33:00 -0700

    update

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -10,12 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 /*
 *	while (i < 1)
 *	{
 *		putchar(i);
 *	}
-*/	printf("Infinite loop avoided! \\o/\n");/* when the for loop is finished */
+*/
+	printf("Infinite loop avoided! \\o/\n");/* when the for loop is finished */
+
 	return (0);
 }
