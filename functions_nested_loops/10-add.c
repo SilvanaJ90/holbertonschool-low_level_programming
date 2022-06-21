2d4b54ccebe8114e11143cc859d463290dd30ac0
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-21 08:05:57 -0700

    update

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - checks
+ *
+ * @j: character to check
+ *
+ * Return: 1 or 0 or -1
+ */
+
+ int add(int num1, int num2)
+	{
+
+	int num3;
+	num1 = 89;
+	num2 = 9;
+	num3 =(num1 + num2);
+
+	return(num3);
+	}
