3ccb53aca6d038237d1f15741f51b0f81ec8f63c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-22 09:20:23 -0700

    update

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ *int _isupper -check
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+int _isupper(int c)
+{
+		if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+
+		return (0);
+
+
+}
