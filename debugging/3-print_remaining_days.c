71cbea49afdc1688b13c03c86d9cd350b91dbaac
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-21 23:12:35 -0700

    update

--- debugging/3-print_remaining_days.c
+++ debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-	if (month >= 1 && day >= 29)
+	if (month == 1 && day >= 30)
 	{
 		day++;
 	}
@@ -24,9 +24,9 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-	if (month == 1 && day == 30)
+	if (month >= 1 && day >=30)
 	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 30, year);
+	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
 	else
 	{
