f0040581e722305df7700054cc6be2257b8eaae1
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-21 22:40:16 -0700

    update

--- debugging/3-main_b.c
+++ debugging/3-main_b.c
@@ -17,9 +17,9 @@ int main(void)
 	day = 29;
     	year = 2000;
 
-    	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	day = convert_day(month, day);
 
-	printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-
+	print_remaining_days(month, day, year);
+    
     return (0);
 }
