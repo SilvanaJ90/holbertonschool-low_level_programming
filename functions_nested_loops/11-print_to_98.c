6d54ed8be809b5faee75c89f08491619deb0dd79
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-21 21:01:13 -0700

    update

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -19,6 +19,7 @@ void print_to_98(int n)
 			if (n != 98)
 			{
 				printf(",");
+				printf(" ");
 			}
 			n--;
 		}
@@ -31,6 +32,7 @@ void print_to_98(int n)
 			if (n != 98)
 			{
 				printf(",");
+				printf(" ");
 			}
 			n++;
 		}
