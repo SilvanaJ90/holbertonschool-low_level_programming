93fb74f4625744aa4720ccbf0f1ccd46bae3254e
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-22 16:07:36 -0700

    update

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - check code
+ */
+
+void print_most_numbers(void)
+
+{
+
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+		_putchar(n);
+		}
+	}
+	_putchar('\n');
+
+}
