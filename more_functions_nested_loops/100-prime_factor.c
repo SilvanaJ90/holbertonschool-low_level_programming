50eb4fede0e630f62f1c6cb157f2a452dcd2bf91
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-23 15:13:56 -0700

    update

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main -check
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	long num = 612852475143;
+
+	for (n = (int) sqrt(num); n > 2; n++)
+	{
+		if (num % n == 0)
+		{
+			printf("%d\n", n);
+			break;
+		}
+	}
+
+	return (0);
+}
