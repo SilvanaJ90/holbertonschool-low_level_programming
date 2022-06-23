363dce37978ad56dbad9b13920a4f9954152d196
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-23 10:06:22 -0700

    update

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - checks
+ *
+ * @size: value
+ *
+ */
+
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	j = 0;
+
+	if (size < 1)
+	_putchar('\n');
+	while (j < size)
+	{
+		i = 0;
+
+		while (i < size)
+		{
+			_putchar('#');
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
