7b6d1de56638b5e2eb060f5b3f5d4ecdd6cd4baf
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-27 16:25:41 -0700

    update

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts: stdout up to but not including the null
+ *
+ * 
+ *
+ * Return: the strlen function return of bytes in the string pointed to by s
+ */
+
+void _puts(char *str)
+{
+puts(str1);
+}
