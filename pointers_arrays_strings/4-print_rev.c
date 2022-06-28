27b7320f7399ed7636200b0d84f92532bdc8ad32
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-28 16:03:47 -0700

    update

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,26 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>   
+#include <string.h>
 
 /**
  * print_rev - check the code
  *
- * @i: -check
+ * @s: pointer
  */
 
 void print_rev(char *s)
 {
-    int len, i, temp;
-    
-    len = strlen(s);
- 
-   
-    for (i = 0; i < len/2; i++) {
-     
-     temp = s[i];
-     s[i] = s[len -i -1];
-     s[len-i-1] = temp;
- 
-    }
+	int i;
+	int c = -1;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		c = c + 1;
+	}
+	while (c >= 0)
+	{
+		_putchar(s[c]);
+		c--;
+	}
+
 }
