679c9a1b22ba64c9352a9e432c55292ba23cc00f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-27 18:22:10 -0700

    update

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>  
+#include <string.h>   
 
 /**
  * print_rev - check the code
@@ -10,13 +9,16 @@
 
 void print_rev(char *s)
 {
-	int i, len, temp;
-	len = strlen(s);
-
-	for(i = 0; i < len/2; i++)
-	{
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
-	}
+    int len, i, temp;
+    
+    len = strlen(s);
+ 
+   
+    for (i = 0; i < len/2; i++) {
+     
+     temp = s[i];
+     s[i] = s[len -i -1];
+     s[len-i-1] = temp;
+ 
+    }
 }
