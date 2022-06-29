c8f01c23487ffd23ab9fd58a88313daf33352af4
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-29 11:27:18 -0700

    update

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - check the code for
+ *
+ * @a: value.
+ *
+ * @n: value.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	putchar('\n');
+}
+
