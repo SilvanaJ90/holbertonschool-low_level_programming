4f7f283ac60a4daa54d54ee6d1d7ddc5f3e9c690
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-30 09:03:25 -0700

    update

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,8 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	for ( ; *s1 == *s2; s1++, s2++)
-		if(*s1==0)
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+		if(s1[i] == s2[i])
 			return (0);
 	return *(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1;
 }
