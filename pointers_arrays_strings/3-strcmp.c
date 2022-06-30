3a75dbc3a9602d5b56d84f283c5c03b0e38e6258
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-30 09:22:03 -0700

    update

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * _strcmp: compara dos cadenas
+ * _strcmp - check code compara dos cadenas
  * @s1: cadena 1
  * @s2: cadena 2
- * Return: Always 0 si las cadenas son iguales, return entero negativo si s1 < s2 o positivo en caso contrario
+ * Return: Always 0 si == s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; i != '\0'; i++)
-		if(s1[i] == s2[i])
-			return (0);
-	return *(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1;
+	for (i = 0; s2[i] != '\0' && s1[i] != 0; i++)
+	{
+		if (s1[i] != s2[i])
+			break;
+	}
+	return (s1[i] - s2[i]);
 }
 
