283d649ff2c1824befc1ba6728a43ad1e348b324
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-06-30 23:30:40 -0700

    update

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,10 +4,12 @@
 #include <ctype.h>
 
 /**
- * cap_string: 1 letter en m
- * @
+ * cap_string - check the code
  *
+ * @str: array
+ * Description: min to may
  *
+ * Return: char
  */
 
 char *cap_string(char *str)
@@ -16,20 +18,20 @@ char *cap_string(char *str)
 	int j;
 	int x = 0;
 	char *c = " \t\n;,.!?\"(){}\0";
-	
+
 	if (str[x] >= 97 && str[x] <= 122)
 	{
 		str[x] = str[x] - 32;
 	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for(j = 0; c[j] != '\0'; j++)
+		for (j = 0; c[j] != '\0'; j++)
 		{
 			if (str[i] == c[j])
 			{
-				if(str[i + 1] >= 97 && str[i + 1] <= 122)
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
 				{
-					str[i + 1] = str[i + 1] -32;
+					str[i + 1] = str[i + 1] - 32;
 				}
 			}
 		}
