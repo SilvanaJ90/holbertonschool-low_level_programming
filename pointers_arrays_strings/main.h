0d1b0f8c0010cb7e896d34fa38e4030a1693fd5a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-02 20:57:15 -0700

    update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -45,3 +50,8 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strchr(char *s, char c);
+
+void print_chessboard(char (*a)[8]);
+
+
+#endif /* MAIN_H */
