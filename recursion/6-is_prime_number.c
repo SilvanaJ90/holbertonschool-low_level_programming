ee642e30b849f7445c900a5e9699b3004ee88218
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-04 15:12:12 -0700

    update10

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -12,14 +12,15 @@ int is_prime_number(int n)
 {
 	int i;
 
+	for (i = 2; i < n / 2; i++)
+        {
+                if (n % i == 0)
+                        return (0);
+        }
+
 	if (n <= 0 || n == 1)
 		return (0);
 	if (n == 4)
 		return (0);
-	for (i = 2; i < n / 2; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
 	return (1);
 }
