cc53350b696e76b16edd5324c5c36522fb176fd2
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-04 14:46:01 -0700

    update

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check code
+ *
+ * @n: number
+ *
+ * Return: number prime
+ */
+
+int is_prime_number(int n);
+{
+	if (n == 0 || n == 1)
+		return (0);
+	if (n == 4)
+		return (0);
+	for (int i = 2;i < n /2; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
+}
+	
