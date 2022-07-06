c1f82e17aae7ad6bf72b319a7b5ef4080a222f30
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-05 20:31:17 -0700

    update

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ * @argc: value
+ * @argv: value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv)
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
+
