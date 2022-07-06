19f2d3ebcb25880a66a4395ce477e86f998719fd
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-05 19:51:06 -0700

    update

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * argc - check code
+ * argv - check code
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+	       	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
+
