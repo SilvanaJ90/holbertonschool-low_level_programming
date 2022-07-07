8c5616e0316d699b9143be3b5f0ff5af3239ce0b
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-07 12:50:22 -0700

    update

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - check code
+ * @width: ancho de la matriz
+ * @height: largo de la matriz
+ * Return: grid - pointer
+ */
+
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int *) * width);
+		if (grid[i] == NULL)
+		{
+			for (--i; i <= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	return (grid);
+}
