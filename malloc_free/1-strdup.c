d16c3a4bda9dd57651f6c4edfacfa37cf04b25cc
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-06 20:20:45 -0700

    update

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -5,22 +5,22 @@
 /**
  * _strdup - check code
  * @str: pointer
- * Return_ 0 always str
+ * Return: 0 always str
  */
 
 char *_strdup(char *str)
 {
 	int i;
-	char *new_s = malloc(sizeof(*new_s) * strlen(str) + 1);
-		if (str == NULL)
-			return (NULL);
+	char *new_s;
 
-		if (new_s == NULL)
-			return (NULL),
-		
-		for (i = 0; str[i] != '\0'; i++)
-		       new_s[i] = str[i];
+	if (str == 0)
+		return (NULL);
+	new_s = malloc(sizeof(*new_s) * strlen(str) + 1);
+	if (new_s == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		new_s[i] = str[i];
+	}
 		return (new_s);
-
-		free (new_s);
 }
