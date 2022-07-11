15878bb44250e5a5a1e361a57d465fe6ef9310c4
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-11 11:26:50 -0700

    update

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -1,7 +1,3 @@
-#ifndef SIZE_H BUFFER_SIZE
-#define BUFFER_SIZE 1024
-SIZE_H
-	==> BUFFER_SIZE
-	==> 1024
-#include <stdio.h>
-#endif /* SIZE_H */
+#ifndef SIZE
+#define SIZE 1024
+#endif /* SIZE */
