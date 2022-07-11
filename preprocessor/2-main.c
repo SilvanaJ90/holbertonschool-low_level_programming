738afa7f13d14858f321b202a5346acc1cf0169f
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-11 12:08:42 -0700

    update

---
+++ preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *name;
+	name = c;
+	printf("%s\n", name);
+    return (0);
+}
