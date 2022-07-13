d43ce3fd7ce319bf4e5762b9f979a4010ad12642
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-13 07:48:37 -0700

    update

--- structures_typedef/5-main.c
+++ structures_typedef/5-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * main - check the code
@@ -13,6 +12,6 @@ int main(void)
 
     my_dog = new_dog("Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    free(my_dog);
+    free_dog(my_dog);
     return (0);
 }
