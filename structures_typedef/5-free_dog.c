cddf860c5b4f8a9526dfc93dd4c883ce2588318d
SJaramillo90 <silvanajaramillo90@gmail.com>
2022-07-13 07:57:49 -0700

    update5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,10 +1,12 @@
-#include "dog.h"
 #include <stdlib.h>
-
+#include "dog.h"
 /**
- * free_dog - check code
- * @d: value
- */
+  * free_dog - check code
+  *
+  * @d: info of dog
+  *
+  * Description: delet info dog
+  */
 
 void free_dog(dog_t *d)
 {
