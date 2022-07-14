ff2b1859b7ad9244abeb84204a35e508d8a3842a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-14 14:05:56 -0500

    update

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -9,12 +9,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i = 0;
+	size_t i;
 
 	if (action != NULL)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
 	}
-	return (0);
 }
