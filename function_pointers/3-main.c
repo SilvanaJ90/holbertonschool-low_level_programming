9b97fa5b4fae56d4fb5421e6a2726a232573d0e2
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-14 23:57:45 -0500

    update

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if ((*op == '/' && b == 0) || (*op == '%' || b == 0))
+	if ((*op == '/' && b == 0) && (*op == '%' && b == 0))
 	{
 		printf("Error\n");
 		exit(100);
