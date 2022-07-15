59de60d832fcc4f961810b4da4f1cf827937b43a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-15 18:15:38 -0500

    update

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,7 +16,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(largs, n);
 	for (i = 0; i < n; i++)
 	{
-	{
 		if (separator != NULL)
 		{
 			printf("%d%s", va_arg(largs, int), separator);
