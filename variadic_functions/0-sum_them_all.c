481fd92a652d3310f91ac91d5e16be91d47e1bfa
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-15 12:35:04 -0500

    update

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -4,18 +4,18 @@
 /**
 * sum_them_all - check code
 * @n: value
-*
+* Return: result
 */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	int result = 0;
-	unsigned int i= 0;
+	unsigned int i = 0;
 	va_list args;
 	va_start(args, n);
 	if (n == 0)
 		return (0);
-	for (i = 0; i < n; i ++)
+	for (i = 0; i < n; i++)
 	{
 		result += va_arg(args, int);
 	}
