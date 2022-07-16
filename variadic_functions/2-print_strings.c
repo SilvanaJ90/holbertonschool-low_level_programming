dd4cadcbc06e10d31041affb6573c5b66b137ecf
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-16 10:34:44 -0500

    update

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <string.h>
+
+/**
+ * print_numbers - check code
+ * @separator: pointer
+ * @n: value
+ * Return: print
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list largs;
+
+	va_start(largs, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(largs, char*));
+
+		if (separator != NULL && i != (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(largs);
+	printf("\n");
+}
