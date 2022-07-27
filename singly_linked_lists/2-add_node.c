fa014b2426828a87c72c6f28a6b72e344bcca2e5
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-27 12:02:58 -0500

    update

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,19 +4,21 @@
 #include <string.h>
 
 /**
- *
- *
- *
+ * add_node - check code
+ * @head: pointer first  likend list
+ * *str: pointer the functions
+ * Return: pointer
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-		list_t *ptr = malloc(sizeof(list_t));
-		if (ptr == NULL)
-				return (NULL);
-		ptr->str = strdup(str);
-		ptr->len = strlen(ptr->str);
-		ptr->next = *head;
-		*head = ptr;
-		return (ptr);
+	list_t *ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+			return (NULL);
+	ptr->str = strdup(str);
+	ptr->len = strlen(ptr->str);
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
 }
