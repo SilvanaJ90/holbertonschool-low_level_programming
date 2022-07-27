c69c4d2d9213104ef8f13d17ee0a2eb42cf6311f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-27 12:04:45 -0500

    error-betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,7 +6,7 @@
 /**
  * add_node - check code
  * @head: pointer first  likend list
- * *str: pointer the functions
+ * str: pointer the functions
  * Return: pointer
  */
 
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *ptr = malloc(sizeof(list_t));
 
 	if (ptr == NULL)
-			return (NULL);
+		return (NULL);
 	ptr->str = strdup(str);
 	ptr->len = strlen(ptr->str);
 	ptr->next = *head;
