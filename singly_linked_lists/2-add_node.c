0b1fe48a0479d05a154f809e31a100a995750489
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-27 12:45:19 -0500

    update

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -19,7 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 	ptr->str = strdup(str);
 	if (ptr->str == NULL)
 		free(ptr);
-		return (NULL);
+	return (NULL);
 	ptr->len = strlen(ptr->str);
 	ptr->next = *head;
 	*head = ptr;
