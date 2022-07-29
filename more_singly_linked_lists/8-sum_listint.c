20a253964657e563aeebfaada9da69fb60333a22
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-29 17:53:40 -0500

    update

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	while(head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+	printf("sum = %d", sum);
+}
