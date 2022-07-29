832b8f58bdcea6199d9b2465d1e4a30fc3bbbf01
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-29 14:23:20 -0500

    update

---
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * reverse_listint
+ * @head: pointer
+ * Return: head
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *nextNode, *prevNode, *currentNode;
+
+	prevNode = NULL;
+	currentNode = *head;
+	nextNode = NULL;
+	while (currentNode != NULL)
+	{
+		nextNode = currentNode->next;
+		currentNode->next = prevNode;
+		prevNode = currentNode;
+		currentNode = nextNode;
+	}
+	*head = prevNode;
+	return (*head);
+}
--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -13,9 +13,7 @@ void free_listint2(listint_t **head)
 	listint_t *currentNode, *nextNode;
 
 	currentNode = *head;
-	if (head == NULL || *head == NULL)
-		return;
-	while (currentNode != NULL)
+	while (currentNode)
 	{
 		nextNode = currentNode->next;
 		free(currentNode);
