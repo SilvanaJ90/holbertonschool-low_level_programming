c7c9dbd00bdf410953ff97ea89cccac275bb262a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-29 11:48:49 -0500

    update

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -13,9 +13,9 @@ void free_listint2(listint_t **head)
 	listint_t *currentNode, *nextNode;
 
 	currentNode = *head;
-	if (currentNode == NULL)
+	if (head == NULL || *head == NULL)
 		return;
-	while (currentNode != NULL)
+	while (currentNode)
 	{
 		nextNode = currentNode->next;
 		free(currentNode);
