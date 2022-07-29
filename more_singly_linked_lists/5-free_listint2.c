8e1ea7c4dd29dd600b9f2021fbcfef475f989aed
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-28 23:52:30 -0500

    update5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_listint - check code
+ * @head: pointer
+ * Retunt:
+ */
+
+void free_listint2(listint_t **head);
+{
+	listint_t *currentNode, *nextNode;
+
+	currentNode = *head;
+	while (currentNode)
+	{
+		nextNode = currentNode->next;
+		free(currentNode);
+		currentNode = nextNode;
+	}
+	*head = NULL;
+}
