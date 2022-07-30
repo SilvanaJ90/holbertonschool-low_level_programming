d0443819547d30fd2cb49ebaf126d87dcb641144
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-29 19:19:46 -0500

    update7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -4,14 +4,15 @@
 #include <assert.h>
 
 /**
- *
- *
- *
+ * get_nodeint_at_index - check code
+ * @head: pointer
+ * @index: value
+ * Return: NULL
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *currentNode= head;
+	listint_t *currentNode = head;
 	unsigned int count = 0;
 
 	while (currentNode != NULL)
