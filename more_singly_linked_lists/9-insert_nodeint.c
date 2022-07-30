b98b3228c957a6cdef5af4cb5b5d1514584e22f4
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-29 23:29:05 -0500

    update

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	newNode->n = n;
 
-	if(idx == 0)
+	if (idx == 0)
 	{
 		newNode->next = temp;
 		*head = newNode;
