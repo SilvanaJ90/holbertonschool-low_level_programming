0c1cd256276364c54a635cbf88c1473eb5c58ca2
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-29 23:23:47 -0500

    update10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index - check code
+ * @head: pointer
+ * @index: position
+ * Return: NULL
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *nextNode, *temp = *head;
+	unsigned int i;
+
+	if (!head || *head)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+	for (i = 0; i < (index - 1); i++)
+	{
+		temp = temp->next;
+		if (temp == NULL || temp->next == NULL)
+			return (-1);
+	}
+	nextNode = temp->next;
+	temp->next = nextNode->next;
+	free(nextNode);
+	return (1);
+}
