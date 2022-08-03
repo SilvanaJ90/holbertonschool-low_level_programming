23ec54650985a880180f2c68f83ae47163ff2e42
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-03 16:31:19 -0500

    update

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - check code
+ * @filename: value
+ * @letters: number of letters it should read and print
+ * Return: 0 or 1
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, sz, ret; /*Descriptor of the file from where the information*/
+	char *buf; /*buffer array in which the read data should be save*/
+
+	buf = malloc(sizeof(char) * letters);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	sz = read(fd, buf, letters);
+	close(fd);
+	if (sz == -1)
+		return (0);
+	ret = write(STDERR_FILENO, buf, sz);
+	free(buf);
+	if (ret != sz)
+		return (0);
+	return (sz);
+}
