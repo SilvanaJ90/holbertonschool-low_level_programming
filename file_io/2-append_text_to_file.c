9d8e92c84d454b2c853c30935929800ecafabd6c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-04 15:24:43 -0500

    update

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * append_text_to_file - check code
+ * @filename: value
+ * @text_content: number of letters it should read and print
+ * Return: 0 or 1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	fd = open(filename, O_WRONLY | O_APPEN);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[len] != '\0')
+		len++;
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
