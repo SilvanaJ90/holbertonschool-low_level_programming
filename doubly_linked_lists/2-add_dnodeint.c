d469c2189e3601440683122483339913029656ba
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-05 13:43:00 -0500

    update

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * add_dnodeint - check code
+ * @head: doble pointer
+ * @n: data
+ * Return: pointer
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode;
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	newNode->prev = NULL;
+	newNode->next = *head;
+	if (*head != NULL)
+		(*head)->prev=newNode;
+	return (newNode);
+}
