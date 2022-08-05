35dc6d95e46e82ff569fb2b0448250332c10bfe1
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-04 21:58:35 -0500

    update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,7 +3,33 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "main.h"
+
+void error97 (void)
+{
+   dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+   exit(97);
+}
+
+void error98 (char *f)
+{
+   dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f);
+   exit(98);
+}
+
+void error99 (char *f)
+{
+   dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f);
+   exit(99);
+}
+
+void error100 (char *f)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", f);
+	exit(100);
+}
 
 /**
  * main - check the code
@@ -11,27 +37,37 @@
  * Return: Always 0.
  */
 
-int main()
+int main (int argc, char **argv)
 {
-   char ch;
-   FILE *source, *target;
-   char file_from[]="x1.txt";
-   char file_to[]="x2.txt";
-   source = fopen(file_from, "r");
-   if (source == NULL) {
-      printf("Press any key to exit...\n");
-      exit(EXIT_FAILURE);
-   }
-   target = fopen(file_to, "w");
-   if (target == NULL) {
-      fclose(source);
-      printf("Press any key to exit...\n");
-      exit(EXIT_FAILURE);
-   }
-   while ((ch = fgetc(source)) != EOF)
-      fputc(ch, target);
-   printf("File copied successfully.\n");
-   fclose(source);
-   fclose(target);
-   return 0;
+	int source, target, sz, ret, closet, closes;
+	char *file_from = argv[1];
+	char *file_to = argv[2];
+	char buf[1024];
+
+	if (argc != 3)
+	   error97();
+
+	source = open(file_from, O_RDONLY);
+	if (source == -1)
+	   error98(file_from);
+	target = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (target == -1)
+		error99(file_to);
+	do {
+	   sz = read(source, buf, 1024);
+	   if (sz == -1)
+		  error98(file_from);
+	   if (sz == 0)
+		  break;
+	   ret = write(target, buf, sz);
+	   if (ret == -1)
+		  error99(file_to);
+	} while (1);
+	closet = close(target);
+	if (closet == -1)
+		error100(file_to);
+	closes = close (source);
+	if (closes == -1)
+		error100(file_from);
+	return (0);
 }
