919a16edb1557f7bfdef2f1f5a22f30ee9b323c7
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-05 13:45:56 -0500

    update2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->prev = NULL;
 	newNode->next = *head;
 	if (*head != NULL)
-		(*head)->prev=newNode;
+		(*head)->prev = newNode;
 	*head = newNode;
 	return (newNode);
 }
