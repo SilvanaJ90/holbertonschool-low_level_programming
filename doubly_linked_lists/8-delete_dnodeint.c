86c518aa4440715a3b9da6754875c41d1a79836a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-06 20:48:14 -0500

    update8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -32,8 +32,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		temp2 = temp->prev;
 		temp2->next = temp->next;
 		temp->next->prev = temp2;
-		free(temp);
 		temp = NULL;
 	}
+	free(temp);
 	return (1);
 }
