cd3eafbf5a7153e94998f18c125a16394c920434
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-08 15:31:29 -0500

    update

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -14,11 +14,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tempNode = *head;
 	dlistint_t *nextNode;
 	dlistint_t *prevNode = NULL;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (*head == NULL)
 		return (-1);
-	for (i = 0; tempNode && i < index; i++)
+	for (i = 0;tempNode && i < index; i++)
 	{
 		prevNode = tempNode;
 		tempNode = tempNode->next;
@@ -40,6 +40,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		}
 	}
 	else if (nextNode == NULL)
+	{
+		prevNode->next = NULL;
+		free(tempNode);
+	}
+	else
 	{
 		prevNode->next = nextNode;
 		nextNode->prev = prevNode;
