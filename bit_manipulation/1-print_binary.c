b9be467e670ed38e9ea679616f83c4dc2d7481e9
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-08 11:42:13 -0500

    update

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_binary - check code
+ * @n: value
+ */
+
+void print_binary(unsigned long int n)
+{
+	if(n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + 48);
+}
