eaeb42309245e6e9150be938b921d28e983f2f9c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-08 11:36:25 -0500

    update

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
  * clear_bit - check code
  * @n: pointer number
  * @index: position node
- * Retrurn: 1
+ * Return: 1
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
@@ -14,7 +14,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long a;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return(-1);
+		return (-1);
 	a = ~(1 << index);
 	*n = *n & a;
 	return (1);
