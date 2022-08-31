5ceb0b6ba077dceff739bc4abc306929f745592a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-30 23:49:25 -0500

    update-0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,36 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * create_item - check code
- * @key: key
- * @value: value
- * Return: pointer to the newly create hash table or NULL is failure
- */
-
-hash_node_t *create_item(char *key, char *value)
-{
-	hash_node_t *item = (hash_node_t *) malloc(sizeof(hash_node_t));
-
-	if (item == NULL)
-		return (NULL);
-	item->key = (char *) malloc(strlen(key) + 1);
-	item->value = (char *) malloc(strlen(value) + 1);
-
-	strcpy(item->key, key);
-	strcpy(item->value, value);
-
-	free(item->key);
-	free(item->value);
-	free(item);
-	return (item);
-}
 
 /**
  * hash_table_create - check code
  * @size: size of the array
  * Return: pointer to the newly create hash table or NULL is failure
  */
+
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
