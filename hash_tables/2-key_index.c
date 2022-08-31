862c342e1335fa6bb7773800600f8fb107b2e015
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-30 23:42:09 -0500

    update-2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -11,7 +11,9 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int idx = hash_djb2((unsigned char *)key);
+	unsigned long int idx = 0;
+
+	idx = hash_djb2((unsigned char *)key);
 
 	return (idx % size);
 }
