cb6bfc3b70f023c0bc398d0425009898fac94180
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-31 10:12:43 -0500

    update-0-create_table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,7 +14,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	table->size = size;
 	table->array = malloc(sizeof(hash_table_t *) * size);
-	free(table->array);
-	free(table);
 	return (table);
 }
