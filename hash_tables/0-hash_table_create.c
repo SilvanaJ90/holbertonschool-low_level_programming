26569f40e555e64fe59cfcd59af24d99ef9a921e
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-30 23:57:57 -0500

    update-0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,12 +14,15 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t *table = NULL;
 
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
+	table->array = calloc(table->size, sizeof(hash_node_t *));
+	if (!table->array)
+		return (NULL);
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 	free(table->array);
