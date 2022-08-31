d39d9b6dfeda1138918a20f971364b0a87810ac8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-31 10:09:35 -0500

    update-0-create_table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_create - check code
+ * @size: size of the array hash table
+ * Return: new pointer
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_table_t *) * size);
+	free(table->array);
+	free(table);
+	return (table);
+}
