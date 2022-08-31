a6b0d8d00a1120be08100d48ea8709ff03ec8476
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-31 00:12:54 -0500

    update-error

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,9 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t **) calloc (table->size, sizeof(hash_node_t *));
-	if (table->array == NULL)
-		return (NULL);
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 	free(table->array);
