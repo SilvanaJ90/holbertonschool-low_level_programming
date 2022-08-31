468f3a0cce62f1e1b55b133fe3831c693e713db8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-31 09:57:49 -0500

    code-modify2

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,17 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * hash_table_create - check code
  * @size: size of the array
  * Return: pointer to the newly create hash table or NULL is failure
  */
 
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table = NULL;
+	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
