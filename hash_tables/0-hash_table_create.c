18cce19a05d8c3613215b42f69fb23a4f9be4a9e
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-31 23:37:58 -0500

    update

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,19 +1,5 @@
 #include "hash_tables.h"
 
-hash_node_t *create_item(const char *key, const char * value)
-{
-	hash_node_t *item = (hash_node_t*) malloc(sizeof(hash_node_t));
-	if (item == NULL)
-		return (NULL);
-	item->key = (char*) malloc (strlen(key) + 1);
-	item->value = (char*) malloc (strlen(value) + 1);
-
-	strcpy(item->key, key);
-	strcpy(item->value, value);
-
-	return (item);
-}
-
 /**
  * hash_table_create - check code
  * @size: size of the array hash table
@@ -22,13 +8,13 @@ hash_node_t *create_item(const char *key, const char * value)
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 	unsigned long int i = 0;
 
-	if (table == NULL)
+	if (ht == NULL)
 		return (NULL);
-	table->size = size;
-	table->array = malloc(sizeof(hash_table_t *) * size);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_table_t *) * size);
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
