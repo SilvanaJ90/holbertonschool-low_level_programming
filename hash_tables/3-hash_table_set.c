cd40701f87105ec2676923591fc0e9e9684130b7
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 02:27:30 -0500

    update-validation

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,19 +10,39 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newNode = NULL, *item = NULL;
+	hash_node_t *newNode = NULL, *item = NULL, *temp = NULL;
 	unsigned long int index = 0;
 
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+	if (!ht || strlen(key) == 0 || !key)
 		return (0);
-
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	item = create_item(key, value);
 	newNode = ht->array[index];
+	temp = newNode;
+	if (!newNode)
+	{
+		item = create_item(key, value);
+		if (!item)
+			return (0);
+		/*item->next = newNode;*/
+		ht->array[index] = item;
+		return (1);
+	}
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 1)
+		{
+			strcpy(ht->array[index]->value, value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	item =  create_item(key, value);
+	if (!item)
+		return (0);
 	item->next = newNode;
-	ht->array[index] = item;
+	newNode = item;
 	return (1);
 }
