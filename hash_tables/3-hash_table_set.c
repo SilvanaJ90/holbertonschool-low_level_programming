dc7e3f56801f0060b13f3e4ac00e4eb68a94b626
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 02:29:16 -0500

    update-validation

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -8,41 +8,62 @@
  * Return: 1 if it succeeded, 0 otherwise
  */
 
+hash_node_t *new_t_node(char *key, char *value)
+{
+    hash_node_t *new_node;
+
+    new_node = malloc(sizeof(hash_node_t));
+    if (!new_node)
+        return (NULL);
+
+    new_node->key = key;
+    new_node->value = value;
+    new_node->next = 0;
+
+    return (new_node);
+}
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newNode = NULL, *item = NULL, *temp = NULL;
-	unsigned long int index = 0;
+    hash_node_t *head, *temp, *new_node;
+    char *val, *k;
+    unsigned long int mapped;
 
-	if (!ht || strlen(key) == 0 || !key)
-		return (0);
-	newNode = malloc(sizeof(hash_node_t));
-	if (newNode == NULL)
-		return (0);
-	index = key_index((unsigned char *)key, ht->size);
-	newNode = ht->array[index];
-	temp = newNode;
-	if (!newNode)
-	{
-		item = create_item(key, value);
-		if (!item)
-			return (0);
-		/*item->next = newNode;*/
-		ht->array[index] = item;
-		return (1);
-	}
-	while (temp)
-	{
-		if (strcmp(temp->key, key) == 1)
-		{
-			strcpy(ht->array[index]->value, value);
-			return (1);
-		}
-		temp = temp->next;
-	}
-	item =  create_item(key, value);
-	if (!item)
-		return (0);
-	item->next = newNode;
-	newNode = item;
-	return (1);
+    if (strlen(key) == 0 || !key || !ht)
+        return (0);
+
+    val = strdup(value);
+    k = strdup(key);
+
+    mapped = key_index((unsigned char *)key, ht->size);
+    head = ht->array[mapped];
+    temp = head;
+
+    if (!head)
+    {
+        new_node = new_t_node(k, val);
+        if (!new_node)
+            return (0);
+        ht->array[mapped] = new_node;
+        return (1);
+    }
+
+    while (temp)
+    {
+        if (strcmp(temp->key, k) == 1)
+        {
+            temp->value = val;
+            return (1);
+        }
+        temp = temp->next;
+    }
+
+    new_node = new_t_node(k, val);
+    if (!new_node)
+        return (0);
+
+    new_node->next = head;
+    head = new_node;
+
+    return (1);
 }
