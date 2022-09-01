1563b242f7b5f443f60c989860d60eaceec53074
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-31 22:45:50 -0500

    update

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -21,7 +21,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, ht->size);
 	newNode = ht->array[index];
 	item = create_item(key, value);
-	item->next = newNode;
-	ht->array[index] = item;
+	if (newNode == NULL)
+	{
+		item->next = newNode;
+		ht->array[index] = item;
+	}
 	return (1);
 }
