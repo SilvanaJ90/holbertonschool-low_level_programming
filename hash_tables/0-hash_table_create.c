4bee1fdb3c5fceaa0dd5086ffc54bd6442e931d4
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 02:51:32 -0500

    update-validation

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,12 +17,9 @@ hash_node_t *create_item(const char *key, const char * value)
 	hash_node_t *item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
 		return (NULL);
-	item->key = malloc (strlen(key) + 1);
-	item->value = malloc (strlen(value) + 1);
-
-	strcpy(item->key, key);
-	strcpy(item->value, value);
-
+	item->key = strdup((char *)key);
+	item->value = strdup((char *)value);
+	item->next = NULL;
 	return (item);
 }
 
