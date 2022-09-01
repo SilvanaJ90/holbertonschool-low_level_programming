5309b48c8966083610797789eed9631f925b4faa
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 01:17:20 -0500

    betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,19 +1,5 @@
 #include "hash_tables.h"
 
-hash_node_t *create_item(const char *key, const char * value)
-{
-	hash_node_t *item = (hash_node_t*) malloc(sizeof(hash_node_t));
-	if (item == NULL)
-		return (NULL);
-	item->key = (char*) malloc (strlen(key) + 1);
-	item->value = (char*) malloc (strlen(value) + 1);
-
-	strcpy(item->key, key);
-	strcpy(item->value, value);
-
-	return (item);
-}
-
 /**
  * hash_table_create - check code
  * @size: size of the array hash table
