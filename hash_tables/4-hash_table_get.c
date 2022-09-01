ceb7d6b0623f250ff0d9c000a7da24e3201e9f1f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 17:40:11 -0500

    update

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,23 +11,20 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
-	hash_node_t *node = NULL;
+	hash_node_t *item = NULL;
 
 	if (!ht || !key)
 		return (NULL);
-	if (strlen(key) == 0)
-		return (NULL);
-
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
-
-	node = ht->array[index];
-	while (node != NULL)
+	item = ht->array[index];
+	/*Ensure that we move to items which are not NULL*/
+	while (item != NULL)
 	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-		node = node->next;
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+		item = item->next;
 	}
 	return (NULL);
 }
