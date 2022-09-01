5b2e24e1d7e00477e30944b443df9dfe49f09f07
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 16:39:29 -0500

    4-hash_table_get-2

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,16 +11,22 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	hash_node_t *item = NULL;
+	hash_node_t *newNode;
 
 	/*Searches the key in the hashtable and returns NULL if it doesn't exis*/
 	index = key_index((const unsigned char *)key, ht->size);
-	item = ht->array[index];
+	newNode = ht->array[index];
 	/* Ensure that we move to a non NULL item*/
-	if (item != NULL)
+	while (newNode != NULL)
 	{
-		if (strcmp(item->key, key) == 0)
-			return (item->value);
+		if (strcmp(newNode->key, key) == 0)
+			return (newNode->value);
+		if (newNode == NULL)
+			return (NULL);
+		else
+		{
+			return (newNode->value);
+		}
 	}
 	return (NULL);
 }
