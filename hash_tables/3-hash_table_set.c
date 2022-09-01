918af1abcee88852f46ec9a260be829aca6b31d3
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-31 20:45:17 -0500

    update-3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,11 +11,13 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newNode = NULL;
+	unsigned long int index = 0;
 
 	if (!ht || !key || !value)
 		return (0);
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
+	index = key_index((unsigned char *)key, ht->size);
 	return (1);
 }
