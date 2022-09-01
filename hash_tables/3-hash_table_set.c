08099b985103b637bca4238eda2b35e7dbf91c99
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 03:39:47 -0500

    update-validation

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,11 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - check code
- * @ht: value hash table
- * @key: value key
- * @value: value
- * Return: 1 if it succeeded, 0 otherwise
+ * free_item - check code
+ * @item: itme
  */
 
 void free_item(hash_node_t *item) {
@@ -14,12 +11,21 @@ void free_item(hash_node_t *item) {
     free(item);
 }
 
+/**
+ * hash_table_set - check code
+ * @ht: value hash table
+ * @key: value key
+ * @value: value
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newNode = NULL, *item = NULL;
+	hash_node_t *newNode, *item;
 	unsigned long int index = 0;
 
-	if (!ht || !key || !value)
+
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	item = malloc(sizeof(hash_node_t));
@@ -37,7 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			item->next = newNode->next;
 			ht->array[index] = item;
-			free_item(item);
+			free_item(newNode);
 			return (1);
 		}
 		while (newNode->next !=NULL && strcmp(newNode->next->key, key) != 0)
