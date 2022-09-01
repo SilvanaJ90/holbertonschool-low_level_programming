8518087c6497dde1f44b807f4802c9c8288475de
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 10:08:15 -0500

    3-hast_table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *item, *newNode;
 
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+	if (!ht || !key || !value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	item = create_item(key, value);
