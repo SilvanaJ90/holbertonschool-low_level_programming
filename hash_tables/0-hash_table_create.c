721c9bb731c26766f675313b945580699c133d4a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 02:00:22 -0500

    update-validation

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,6 +2,27 @@
 
 /**
  * hash_table_create - check code
+ * @size: size of the array
+ * Return: pointer to the newly create hash table or NULL is failure
+ */
+
+hash_node_t *create_item(const char *key, const char * value)
+{
+	hash_node_t *item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
+		return (NULL);
+	item->key = malloc (strlen(key) + 1);
+	item->value = malloc (strlen(value) + 1);
+
+	strcpy(item->key, key);
+	strcpy(item->value, value);
+
+	return (item);
+}
+
+
+/**
+ * hash_table_create - check code
  * @size: size of the array hash table
  * Return: new pointer
  */
--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -8,67 +8,41 @@
  * Return: 1 if it succeeded, 0 otherwise
  */
 
-/**
- * free_node - Free a node.
- * @node: Node to free.
- *
- * Return: Void.
- */
-void free_node(hash_node_t *node)
-{
-	free(node->key);
-	free(node->value);
-	free(node);
-}
-
-/**
- * hash_table_set - Set a value in the hash table.
- * @ht: Hash table.
- * @key: Key to be indexed.
- * @value: Value to set in the hash table.
- *
- * Return: 1 if works, 0 if doesn't.
- */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *current;
+	hash_node_t *newNode = NULL, *item = NULL, *temp = NULL;
+	unsigned long int index = 0;
 
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+	if (!ht || strlen(key) == 0 || !key)
 		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	newNode = malloc(sizeof(hash_node_t));
+	if (newNode == NULL)
 		return (0);
-	new_node->key = strdup((char *)key);
-	new_node->value = strdup((char *)value);
-	new_node->next = NULL;
-	if (ht->array[index] == NULL)
-		ht->array[index] = new_node;
-	else
+	index = key_index((unsigned char *)key, ht->size);
+	newNode = ht->array[index];
+	temp = newNode;
+	if (!newNode)
 	{
-		current = ht->array[index];
-		if (strcmp(current->key, key) == 0)
+		item = create_item(key, value);
+		if (!item)
+			return (0);
+		/*item->next = newNode;*/
+		ht->array[index] = item;
+		return (1);
+	}
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 1)
 		{
-			new_node->next = current->next;
-			ht->array[index] = new_node;
-			free_node(current);
+			strcpy(ht->array[index]->value, value);
 			return (1);
 		}
-		while (current->next != NULL && strcmp(current->next->key, key) != 0)
-		{ current = current->next;
-		}
-		if (strcmp(current->key, key) == 0)
-		{
-			new_node->next = current->next->next;
-			free_node(current->next);
-			current->next = new_node;
-		}
-		else
-		{
-			new_node->next = ht->array[index];
-			ht->array[index] = new_node;
-		}
+		temp = temp->next;
 	}
+	item =  create_item(key, value);
+	if (!item)
+		return (0);
+	item->next = newNode;
+	newNode = item;
 	return (1);
 }
