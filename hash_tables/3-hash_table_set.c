7f44706a4dca1d7c73969786f1889b654be46da9
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 03:22:21 -0500

    update-validation

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -32,11 +32,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || !value)
 		return (0);
-	newNode = malloc(sizeof(hash_node_t));
-	if (newNode == NULL)
-		return (0);
-	item = create_item(key, value);
 	index = key_index((unsigned char *)key, ht->size);
+	item = create_item(key, value);
 	if (ht->array[index] != NULL)
 		ht->array[index] = item;
 	else
