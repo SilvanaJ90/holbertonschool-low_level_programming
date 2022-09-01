af7d1cf5e6b8880391141ee7bb0eb51aa8cdbb7d
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 02:06:41 -0500

    update-validation

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,39 +10,17 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newNode = NULL, *item = NULL, *temp = NULL;
+	hash_node_t *newNode = NULL, *item = NULL;
 	unsigned long int index = 0;
 
-	if (!ht || strlen(key) == 0 || !key)
+	if (!ht || !key || !value)
 		return (0);
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	newNode = ht->array[index];
-	temp = newNode;
-	if (!newNode)
-	{
-		item = create_item(key, value);
-		if (!item)
-			return (0);
-		/*item->next = newNode;*/
-		ht->array[index] = item;
-		return (1);
-	}
-	while (temp)
-	{
-		if (strcmp(temp->key, key) == 1)
-		{
-			strcpy(ht->array[index]->value, value);
-			return (1);
-		}
-		temp = temp->next;
-	}
-	item =  create_item(key, value);
-	if (!item)
-		return (0);
 	item->next = newNode;
-	newNode = item;
+	ht->array[index] = item;
 	return (1);
 }
