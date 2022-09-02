a64c0ae302fdd7b0287f9accc735a6bc5e847587
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 20:44:31 -0500

    update

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -8,22 +8,23 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
-	hash_node_t *item = NULL;
+	hash_node_t *tmp = NULL;
 	int flag = 0;
 
 	if (!ht || !ht->array)
 		return;
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		item = ht->array[i];
-		while (item != NULL)
+		tmp = ht->array[i];
+		while (tmp != NULL)
 		{
 			if (flag == 1)
 				printf(", ");
-			printf("'%s':''%s'", item->key, item->value);
+			printf("'%s': '%s'", tmp->key, tmp->value);
 			flag = 1;
-			item = item->next;
+			tmp = tmp->next;
 		}
 	}
 	printf("}\n");
