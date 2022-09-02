17dede6b0871c42268bd1c73c1e0d5a33043bc5a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-02 00:07:50 -0500

    update-6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -8,17 +8,19 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
-	/*hash_node_t *item = NULL;*/
 
 	if (!ht || !ht->array)
 		return;
-
-    for (i = 0; i <ht->size; i++) {
-        hash_node_t *item = ht->array[i];
-        if (item != NULL)
-            free_item(item);
-    }
-
-    free(ht->array);
-    free(ht);
+	for (i = 0; i <ht->size; i++)
+	{
+		hash_node_t *item = ht->array[i];
+		if (item != NULL)
+		{
+			free_item(item);
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
 }
