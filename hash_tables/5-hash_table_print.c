584f0ec36d946a81f05b51555163a7cd3c1a1694
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-01 19:00:00 -0500

    5-print hash table

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -13,9 +13,10 @@ void hash_table_print(const hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
-		{
 			printf("%s%s\n", ht->array[i]->key, ht->array[i]->value);
+		else
+		{
+			return;
 		}
 	}
-	return;
 }
